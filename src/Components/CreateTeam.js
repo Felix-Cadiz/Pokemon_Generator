33752388fccd7d537be2e151d7f9b7a52b508186
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-02-26 09:23:51 -0800

    Type Dropdown Functional

--- src/Components/CreateTeam.js
+++ src/Components/CreateTeam.js
@@ -1,12 +1,42 @@
-import React from "react";
+import {React, useEffect, useState} from "react";
+import Dropdown from "react-dropdown";
 import "./CreateTeam.css"
 
 const CreateTeam = () => {
+
+    const [type, setType] = useState(1);
+    const [typeOptions, setTypeOptions] = useState();
+
+    const typeURL = `https://pokeapi.co/api/v2/type/`
+
+    const fetchTypes = async () => {
+
+        try {
+            const response = await fetch(typeURL)
+            const result = await response.json()
+            const specificTypes = result.results
+            console.log(result)
+            console.log(specificTypes)
+            const individualType = specificTypes.map((specificType) => {
+                return specificType.name
+            })
+            console.log(individualType)
+            setTypeOptions(individualType)
+
+
+        } catch (error) {
+            throw error
+        }
+    }
+
+    useEffect(fetchTypes, [])
+
     return <>
+    
     <h1> Sup CreateTeam</h1>
 
     <button> Random Team </button>
-    <button> Gym Leader </button>
+    <Dropdown options={typeOptions} placeholder="Gym Leader"></Dropdown>
 
     <div className="body">
             <div className="container" onClick={() => {
